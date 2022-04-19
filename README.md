f7512eb02bd42ff00ec1f8c786a7b26b052b4618
npaezsar <npaezsar@correo.uis.edu.co>
2022-04-19 14:34:13 -0500

    Desarrollo: programa completo. Diagrama y codigo. npaezsar

---
+++ README.md
@@ -0,0 +1,6 @@
+# Ejercicio No. 1: SUMA DE DOS NUMEROS ENTEROS.
+
+## Definición de variables
+x: primer numeros 
+y: segundo numero 
+z: la suma de x e y
---
+++ suma_enteros.py
@@ -0,0 +1,16 @@
+"""EJERCICIO No. 1:
+Suma de dos numeros enteros."""
+
+print("---------------------")
+print("-----SUMA ENTEROS----")
+print("---------------------")
+
+# input
+x = int(input("Digite el valor de x: "))
+y = int(input("Digite el valor de y: "))
+
+# processing
+z = x + y
+
+# output
+print("La suma de " + str(x) + " y " + str(y) + " es " + str(z))
